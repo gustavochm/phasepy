6d281ceba8c18c6a562f8a1c0adee15e4e347390
gustavo <gustavoch@outlook.com>
2019-03-02 18:12:36 -0300

    Change long description

---
+++ long_description.rst
@@ -0,0 +1,10 @@
+Phasepy is a open-source scientific python package for fluid phase equilibria computation.
+This package facilitate the calculation of liquid-vapour equilibrium, liquid-liquid equilibrium
+and liquid-liquid-vapour equilibrium. Equilibrium calculations can be perfomed with cubic equations
+of state with clasic or advances mixing rules or with a discontinuous approach using a virial equations
+of state for the vapour phase and a activity coefficient model for the liquid phase.
+
+Besides computations, with this package is also possible to fit phase equilibria data, functions to fit quadratic
+mix rule, NRTL, Wilson and Redlich Kister parameters, are included.
+
+Phasety relys on numpy, scipy and cython extension modules, when necessary.
--- setup.py
+++ setup.py
@@ -20,27 +20,16 @@ else:
                    Extension('phasepy.actmodels.actmodels_cy', ['phasepy/src/actmodels_cy.c']),
                     Extension('phasepy.sgt.cijmix_cy', ['phasepy/src/cijmix_cy.c'])]
 
-long_description = "Phasepy is a open-source scientific python package for fluid phase equilibria computation.
-This package facilitate the calculation of liquid-vapour equilibrium, liquid-liquid equilibrium
-and liquid-liquid-vapour equilibrium. Equilibrium calculations can be perfomed with cubic equations
-of state with clasic or advances mixing rules or with a discontinuous approach using a virial equations
-of state for the vapour phase and a activity coefficient model for the liquid phase.
-
-Besides computations, with this package is also possible to fit phase equilibria data, functions to fit quadratic
-mix rule, NRTL, Wilson and Redlich Kister parameters, are included.
-
-Phasety relys on numpy, scipy and cython extension modules, when necessary.
-"
 setup(
   name = 'phasepy',
   license='MIT',
-  version = '0.0.4',
+  version = '0.0.5',
   description = 'Multiphase multicomponent Equilibria',
   author = 'Gustavo Chaparro Maldonado, Andrés Mejía Matallana',
   author_email = 'gustavochaparro@udec.cl',
   url = 'https://github.com/gustavochm/phasepy',
   download_url = 'https://github.com/gustavochm/phasepy.git',
-  long_description = long_description,
+  long_description = open('long_description.rst').read(),
   packages = ['phasepy', 'phasepy.cubic', 'phasepy.equilibrium','phasepy.fit', 'phasepy.sgt', 'phasepy.actmodels'],
   cmdclass = cmdclass,
   ext_modules = ext_modules,
