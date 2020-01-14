Fixes in multiflash jacobian

phasepy/equilibrium/multiflash.py
@@ -16,12 +16,13 @@ def multiflash_obj(inc, Z, K):
     betar = beta[1:]
     Kexp = K.T*np.exp(tetha)
     K1 = Kexp - 1. #nc x P - 1 
-    sum1 = 1. + (K1*betar).T.sum(axis=0) # P - 1
+    sum1 = 1 + K1@betar # P - 1
     Xref = Z/sum1
     
     f1 = beta.sum() - 1.
     f2 = (K1.T*Xref).sum(axis=1)
-    f3 = (betar*tetha) / (betar + tetha)
+    betatetha = (betar + tetha)
+    f3 = (betar*tetha) / betatetha
     f = np.hstack([f1,f2,f3])
     
     
@@ -29,20 +30,24 @@ def multiflash_obj(inc, Z, K):
     Id = np.eye(nbeta-1)
     
     #f1 derivative
-    f1db = np.ones_like(beta)
+    f1db = np.ones(nbeta)
     jac[0,:nbeta] = f1db
     
     #f2 derivative
-    df20 = Z*K1.T/sum1**2
-    jac[1:nbeta,1:nbeta ] = - df20@K1
-    aux = df20*(Kexp*betar).T
-    jac[1:nbeta, nbeta:] = Id*((Kexp.T*Z/sum1).T.sum(axis=0))-aux@aux.T
+    ter1 = Xref/sum1 * K1.T 
+    dfb = - ter1@K1 
+    jac[1:nbeta,1:nbeta ] = dfb
+    dft = Id * (Xref*Kexp.T).sum(axis = 1)
+    ter2 = Kexp*betar
+    dft -= ter1 @ ter2
+    jac[1:nbeta, nbeta:] = dft
 
     #f3 derivative
-    f3db =  (tetha / (betar + tetha))**2
-    f3dt = (betar/(betar + tetha))**2
+    f3db =  (tetha / betatetha)**2
+    f3dt = (betar / betatetha)**2
     jac[nbeta:, 1:nbeta] = Id*f3db
     jac[nbeta:, nbeta:] = Id*f3dt
+    
     
     return f, jac, Kexp, Xref
 

phasepy/fit/fitpsat.py
@@ -31,7 +31,7 @@ def fobj_alpha(k,Texp,Pexp,cubic):
     cubic.k = k
     P = np.zeros_like(Pexp)
     for i in range(len(Texp)):
-        P[i] = cubic.psat(Texp[i])
+        P[i], _, _  = cubic.psat(Texp[i])
     return np.sum((P-Pexp)**2)
 
 #fit  SV alpha

phasepy/sgt/path_sk.py
@@ -1,6 +1,6 @@
 from __future__ import division, print_function, absolute_import
 import numpy as np
-from scipy.optimize import fsolve
+from scipy.optimize import fsolve, root
 from scipy.integrate import cumtrapz
 from .cijmix_cy import cmix_cy
 from .tensionresult import TensionResult
@@ -15,7 +15,8 @@ def fobj_sk(inc, spath, T, mu0, ci, sqrtci, model):
     obj[-1] = spath - sqrtci.dot(ro)
     return obj
 
-def ten_beta0_sk(ro1, ro2, Tsat, Psat, model, n = 200, full_output = False ):
+def ten_beta0_sk(ro1, ro2, Tsat, Psat, model, n = 200, full_output = False,
+                 alpha0 = None ):
     
     nc = model.nc
     
@@ -41,11 +42,14 @@ def ten_beta0_sk(ro1, ro2, Tsat, Psat, model, n = 200, full_output = False ):
     ro[:,0] = ro1a
     ro[:,-1] = ro2a
     
-    deltaro = ro2a - ro1a
+    deltaro = ro2a - ro1a   
     i = 1
-    r0 = ro1a + deltaro * (spath[i]-s0)
-    r0 = np.hstack([r0, 0])
-    ro0 = fsolve(fobj_sk,r0,args=(spath[i], Tsat, mu0, ci, sqrtci, model))
+    r0 = ro1a + deltaro * (spath[i]-s0) / n
+    if alpha0 is None:
+        alpha0 = np.mean((model.muad(r0, Tsat) - mu0)/sqrtci)
+    r0 = np.hstack([r0, alpha0])
+    ro0 = root(fobj_sk,r0,args=(spath[i], Tsat, mu0, ci, sqrtci, model), method = 'lm')
+    ro0 = ro0.x
     ro[:,i] = ro0[:-1]
     
     for i in range(1,n):