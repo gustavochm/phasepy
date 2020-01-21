50d80b6041fa2803dc5d2a5a2c50835781ace17e
gustavochm <gustavoch@outlook.com>
2020-01-21 08:40:06 -0300

    Added fitting of volume traslation

---
+++ build/lib.win-amd64-3.7/phasepy/fit/fitvt.py
@@ -0,0 +1,40 @@
+from __future__ import division, print_function, absolute_import
+from scipy.optimize import minimize 
+import numpy as np
+
+
+def fobj_c(c, eos, Texp, Pexp, rhoexp):
+    eos.c = c
+    n = len(Texp)
+    rho = np.zeros_like(rhoexp)
+    for i in range(n):
+        rho[i] = eos.density(Texp[i], Pexp[i], 'L')
+        
+    return np.sum((rho - rhoexp)**2)
+
+def fit_vt(component, eos, Texp, Pexp, rhoexp, c0 = 0.):
+    """
+    fit_vt fit Volume Traslation for cubic EoS 
+    
+    Parameters
+    ----------
+    component : object
+        created with component class
+    eos:  function
+        cubic eos function
+    Texp : array_like
+        experimental temperature in K.
+    Pexp : array_like
+        experimental pressure in bar.
+    ks0 : array_like, optional
+        initial values.
+    
+    Returns
+    -------
+    fit : OptimizeResult
+        Result of SciPy minimize
+    
+    """
+    cubic = eos(component, volume_traslation = True)
+    fit = minimize(fobj_c, c0, args = (cubic, Texp, Pexp, rhoexp))
+    return fit
