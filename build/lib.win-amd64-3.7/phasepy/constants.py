5258af91a0cd56b42736b9f106271ba5a9512162
gustavochm <gustavoch@outlook.com>
2020-02-18 08:21:30 -0300

    Deleted build files

--- build/lib.win-amd64-3.7/phasepy/constants.py
+++
@@ -1,9 +0,0 @@
-from __future__ import division, print_function, absolute_import
-
-# Constants
-kb = 1.3806488e-23 # [J/K] Boltzman's constant
-Na = 6.022142e23  # [mol-1] Avogrado's Number
-
-R = 83.14        #[bar cm3 mol-1 K-1] Ideal gas constant
-r = 0.083144703523     # [bar L mol-1 K-1] Ideal gas constant
-
--- build/lib.win-amd64-3.7/phasepy/saftvrmie/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import division, print_function, absolute_import
-
-#__all__ = [s for s in dir() if not s.startswith('_')]
-
-from .saftvrmie import saftvrmie_pure
