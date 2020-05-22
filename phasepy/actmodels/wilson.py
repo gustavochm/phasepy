c600915a3cff0ac611d654e2df6860b2055675d1
gustavochm <gustavoch@outlook.com>
2020-05-22 16:16:40 -0400

    Updated Wilson Model with derivatives of activity coefficients

--- phasepy/actmodels/wilson.py
+++ phasepy/actmodels/wilson.py
@@ -1,12 +1,12 @@
 from __future__ import division, print_function, absolute_import
 import numpy as np
-from .actmodels_cy import wilson_cy
+from .actmodels_cy import wilson_cy, dwilson_cy
 
 
 def wilson(X, T, A, vl):
     '''
     Wilson activity coefficient model
-    
+
     Parameters
     ----------
     X: array like
@@ -17,7 +17,7 @@ def wilson(X, T, A, vl):
         matrix of energy interactions in K
     vl: function
         liquid volume of species in cm3/mol
-    
+
     Returns
     -------
     lngama: array_like
@@ -28,3 +28,32 @@ def wilson(X, T, A, vl):
     M = np.divide.outer(v, v).T * np.exp(-A/T)
     lngama = wilson_cy(X, M)
     return lngama
+
+def dwilson(X, T, A, vl):
+    '''
+    Derivatives of Wilson activity coefficient model
+
+    Parameters
+    ----------
+    X: array like
+        vector of molar fractions
+    T: float
+        absolute temperature in K
+    A: array like
+        matrix of energy interactions in K
+    vl: function
+        liquid volume of species in cm3/mol
+
+    Returns
+    -------
+    lngama: array_like
+        natural logarithm of activify coefficient
+    dlngama: array_like
+        derivative of natural logarithm of activify coefficient
+    '''
+    X = np.asarray(X,dtype = np.float64)
+    v = vl(T)
+    M = np.divide.outer(v, v).T * np.exp(-A/T)
+    lngama, dlngama = dwilson_cy(X, M)
+
+    return lngama, dlngama
