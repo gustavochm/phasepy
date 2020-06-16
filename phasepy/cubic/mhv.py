1e5812a6031d3088c1c2ca931ef01ee5feb23a41
gustavochm <gustavoch@outlook.com>
2020-06-16 15:25:20 -0400

    Fix unintented changed in mhv

--- phasepy/cubic/mhv.py
+++ phasepy/cubic/mhv.py
@@ -268,7 +268,8 @@ def d2mhv(X, T, ai, bi, c1, c2, ActModel, parameter):
     dap_dnij += R*T*np.outer(dem_dn, dbm_dn)
 
     D = am
-    Di = am + apmn
+    Di = am + ap
+    Dij = dap_dnij +  np.add.outer(ap, ap)
     B = bm
     Bi = bi
     Bij = 0.
