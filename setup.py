cb3aa7965c70914b5eaa83e25b8f285977e9dc4a
gustavochm <gustavoch@outlook.com>
2021-01-07 12:27:03 -0300

    updated version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ else:
 setup(
   name='phasepy',
   license='MIT',
-  version='0.0.45',
+  version='0.0.46',
   description='Multiphase multicomponent Equilibria',
   author='Gustavo Chaparro Maldonado, Andres Mejia Matallana',
   author_email='gustavochaparro@udec.cl',
