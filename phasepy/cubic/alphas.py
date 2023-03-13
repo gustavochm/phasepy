528ac53d43e140acfdc115ad07166dc6da77a365
gustavochm <gustavoch@outlook.com>
2023-03-13 14:58:53 +0000

    added AAT alpha function

--- phasepy/cubic/alphas.py
+++ phasepy/cubic/alphas.py
@@ -24,3 +24,25 @@ def alpha_sv(T, ksv, Tc):
     Tr = T/Tc
     sTr = np.sqrt(Tr)
     return (1+(k0+k1*(0.7-Tr)*(1+sTr))*(1-sTr))**2
+
+
+# Almeida-Aznar-Telles alphas function
+def alpha_aat(T, AAT, Tc):
+    """
+    Parameters
+    ----------
+    T : float
+        Temperature [K]
+    AAT : array_like
+        AAT parameters, AAT = [AAT1, AAT2, AAT3]
+    Tc : float
+        Critical temperature [K]
+    """
+    AAT = AAT.T
+    AAT1 = AAT[0]
+    AAT2 = AAT[1]
+    AAT3 = AAT[2]
+    Tr = T/Tc
+    alpha_aux = AAT1 * (1. - Tr) * np.abs(1. - Tr)**(AAT2 - 1.)
+    alpha_aux += AAT3 * (1/Tr - 1.)
+    return np.exp(alpha_aux)
